8cd03b2c211a887488f285b2e377b8ec799fd4d1
xuscode <song_hsu@126.com>
2021-07-22 15:45:26 +0800

    添加批处理

---
+++ 3DEXPERIENCE_Platform_V6R2019x/01-START_UP/02-CLEAN_PLATFORM.bat
@@ -0,0 +1,9 @@
+
+Rd /s /q C:\DassaultSystemes\R2019x\3DDashboard\win_b64\code\tomee\work\Catalina
+Rd /s /q C:\DassaultSystemes\R2019x\3DPassport\win_b64\code\tomee\work\Catalina
+Rd /s /q C:\DassaultSystemes\R2019x\3DSpace\win_b64\code\tomee\work\Catalina
+Rd /s /q C:\DassaultSystemes\R2019x\3DSpace\win_b64\code\tomeeNoCAS\work\Catalina
+Rd /s /q C:\DassaultSystemes\R2019x\FedSearch\win_b64\code\tomee\work\Catalina
+Rd /s /q C:\DassaultSystemes\R2019x\3DComment\win_b64\code\tomee\work\Catalina
+Rd /s /q C:\DassaultSystemes\R2019x\3DNotification\win_b64\code\tomee\work\Catalina
+Rd /s /q C:\DassaultSystemes\R2019x\3DSwym\win_b64\code\tomee\work\Catalina
