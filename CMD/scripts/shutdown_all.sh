5f1201ec96789fccd151f484869ffc6cc77b086e
xuscode <song_hsu@126.com>
2021-08-05 13:43:15 +0800

    add linux cmd

---
+++ CMD/scripts/shutdown_all.sh
@@ -0,0 +1,17 @@
+/app/DassaultSystemes/R2021x/3DNotification/linux_a64/code/command/node stop
+/app/DassaultSystemes/R2021x/3DNotification/linux_a64/code/command/node stop
+/app/DassaultSystemes/R2021x/3DComment/linux_a64/code/tomee/bin/shutdown.sh
+/app/DassaultSystemes/R2021x/3DSwym/linux_a64/code/tomee/bin/shutdown.sh
+/app/DassaultSystemes/R2021x/3DSwym/linux_a64/datadir/bin/cvinit.sh stop
+/app/DassaultSystemes/R2021x/3DSwym/linux_a64/code/command/ExternalConverterSvc stop
+/app/DassaultSystemes/R2021x/3DSpace/scripts/TomEECAS_Shutdown.sh
+/app/DassaultSystemes/R2021x/3DSpace/scripts/TomEENoCAS_Shutdown.sh
+/app/DassaultSystemes/R2021x/CentralFCS/linux_a64/code/tomee/bin/shutdown.sh
+/app/DassaultSystemes/R2021x/RemoteFCS/linux_a64/code/tomee/bin/shutdown.sh
+/app/DassaultSystemes/R2021x/FedSearch/linux_a64/code/tomee/bin/shutdown.sh
+/app/DassaultSystemes/R2021x/3DDashboard/linux_a64/code/tomee/bin/shutdown.sh
+/app/DassaultSystemes/R2021x/3DPassport/linux_a64/code/tomee/bin/shutdown.sh
+/app/DassaultSystemes/R2021x/3DSpaceIndex/linux_a64/code/command/stopXLAdvancedSearchServer.sh
+sleep 30
+dbshut $ORACLE_HOME
+
