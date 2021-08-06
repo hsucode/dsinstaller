88fe56844578a0483e9fbe2b2d038c174e2f24a5
xuscode <song_hsu@126.com>
2021-08-06 16:56:08 +0800

    upa

--- CMD/SetDSLicense.cmd
+++ CMD/SetDSLicense.cmd
@@ -1,2 +1,2 @@
 md C:\ProgramData\DassaultSystemes\Licenses\
-echo 3907n754w1.zicp.vip:20989>C:\ProgramData\DassaultSystemes\Licenses\DSLicSrv.txt
+echo 3907n754w1.zicp.ooo:20989>C:\ProgramData\DassaultSystemes\Licenses\DSLicSrv.txt
