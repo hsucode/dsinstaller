8e186a636966d07c93a72a5903ed89cb552e4c59
xuscode <song_hsu@126.com>
2021-12-09 10:04:15 +0800

    newer

--- 3DEXPERIENCE_NativeApps_Installer/V5R27/Batch_Installation_Wizard.bat
+++ 3DEXPERIENCE_NativeApps_Installer/V5R27/Batch_Installation_Wizard.bat
@@ -1,11 +1,22 @@
-set MyPath=%~dp0
-goto B27
-:B27
-%MyPath%CATIA_P3.win_b64\1\WIN64\StartB -u "C:\Program Files\Dassault Systemes\B27" -newdir -D "C:\Program Files\Dassault Systemes\B27\CATEnv"  -all -v 
-::%MyPath%DELMIA.win_b64\1\WIN64\StartB -u "C:\Program Files\Dassault Systemes\B27" -newdir -D "C:\Program Files\Dassault Systemes\B27\CATEnv"  -all -v 
-%MyPath%SPK.win_b64\1\WIN64\StartSPKB -bC -killprocess -v
-%MyPath%HOTFIXR27SP6\CDROM\B27.6.39.win_b64\1\WIN64\StartHFXB -killprocess  -v
-%MyPath%CAA_CATIA_P3.win_b64\1\WIN64\StartCAAB -code "C:\Program Files\Dassault Systemes\B27" -all -v
-%MyPath%SPK_CAA.win_b64\1\WIN64\StartSPKB -u "C:\Program Files\Dassault Systemes\B27" -bC -killprocess -v
-%MyPath%RADE.win_b64\1\WIN64\StartB -u "C:\Program Files\Dassault Systemes\B27_RADE" -newdir -D "C:\Program Files\Dassault Systemes\B27_RADE\CATEnv" -ident "RADE" -all -v
-exit
+cd /d %~dp0
+:GA
+CATIA_P3.win_b64\1\WIN64\StartB -u "C:\Program Files\Dassault Systemes\B27" -newdir -D "C:\Program Files\Dassault Systemes\B27\CATEnv"  -all -v -addUserPrivilegesForOrbix
+DELMIA.win_b64\1\WIN64\StartB -u "C:\Program Files\Dassault Systemes\B27" -newdir -D "C:\Program Files\Dassault Systemes\B27\CATEnv"  -all -v 
+
+:SP
+SPK.win_b64\1\WIN64\StartSPKB -u "C:\Program Files\Dassault Systemes\B27" -bC -killprocess -v
+HOTFIXR27SP6\CDROM\B27.6.40.win_b64\1\WIN64\StartHFXB -u "C:\Program Files\Dassault Systemes\B27" -killprocess  -v
+
+:CAA
+CAA_CATIA_P3.win_b64\1\WIN64\StartCAAB -code "C:\Program Files\Dassault Systemes\B27" -all -v
+SPK_CAA.win_b64\1\WIN64\StartSPKB -u "C:\Program Files\Dassault Systemes\B27" -bC -killprocess -v
+
+:RADE
+RADE.win_b64\1\WIN64\StartB -u "C:\Program Files\Dassault Systemes\B27_RADE" -newdir -D "C:\Program Files\Dassault Systemes\B27_RADE\CATEnv" -ident "RADE" -all -v
+SPK.win_b64\1\WIN64\StartSPKB -u "C:\Program Files\Dassault Systemes\B27_RADE" -bC -killprocess -v
+HOTFIXR27SP6\CDROM\B27.6.40.win_b64\1\WIN64\StartHFXB -u "C:\Program Files\Dassault Systemes\B27_RADE" -killprocess  -v
+
+:: "C:\Program Files\Dassault Systemes\B27\win_b64\code\bin\CATSoftwareMgtB" -hfxR
+:: "C:\Program Files\Dassault Systemes\B27_RADE\win_b64\code\bin\CATSoftwareMgtB" -hfxR
+
+pause
--- 3DEXPERIENCE_NativeApps_Installer/V5R29/Batch_Installation_Wizard.bat
+++ 3DEXPERIENCE_NativeApps_Installer/V5R29/Batch_Installation_Wizard.bat
@@ -1,18 +1,22 @@
-set MyPath=%~dp0
-goto B29
-:B29
-%MyPath%CATIA_P3.win_b64\1\WIN64\StartB -u "C:\Program Files\Dassault Systemes\B29" -newdir -D "C:\Program Files\Dassault Systemes\B29\CATEnv"  -all -v 
-%MyPath%DELMIA.win_b64\1\WIN64\StartB -u "C:\Program Files\Dassault Systemes\B29" -newdir -D "C:\Program Files\Dassault Systemes\B29\CATEnv"  -all -v 
-%MyPath%RADE.win_b64\1\WIN64\StartB -u "C:\Program Files\Dassault Systemes\B29_RADE" -newdir -D "C:\Program Files\Dassault Systemes\B29_RADE\CATEnv" -ident "RADE" -all -v
-%MyPath%CAA_CATIA_P3.win_b64\1\WIN64\StartCAAB -code "C:\Program Files\Dassault Systemes\B29" -all -v
+cd /d %~dp0
+:GA
+CATIA_P3.win_b64\1\WIN64\StartB -u "C:\Program Files\Dassault Systemes\B29" -newdir -D "C:\Program Files\Dassault Systemes\B29\CATEnv"  -all -v -addUserPrivilegesForOrbix 
+DELMIA.win_b64\1\WIN64\StartB -u "C:\Program Files\Dassault Systemes\B29" -newdir -D "C:\Program Files\Dassault Systemes\B29\CATEnv"  -all -v 
 
-%MyPath%SPK.win_b64\1\WIN64\StartSPKB -u "C:\Program Files\Dassault Systemes\B29" -bC -killprocess -v
-%MyPath%SPK.win_b64\1\WIN64\StartSPKB -u "C:\Program Files\Dassault Systemes\B29_RADE" -bC -killprocess -v
+:SP
+SPK.win_b64\1\WIN64\StartSPKB -u "C:\Program Files\Dassault Systemes\B29" -bC -killprocess -v
+HOTFIXR29SP6\CDROM\B29.6.8.win_b64\1\WIN64\StartHFXB -u "C:\Program Files\Dassault Systemes\B29" -killprocess  -v
 
-%MyPath%SPK_CAA.win_b64\1\WIN64\StartSPKB -u "C:\Program Files\Dassault Systemes\B29" -bC -killprocess -v
+:CAA
+CAA_CATIA_P3.win_b64\1\WIN64\StartCAAB -code "C:\Program Files\Dassault Systemes\B29" -all -v
+SPK_CAA.win_b64\1\WIN64\StartSPKB -u "C:\Program Files\Dassault Systemes\B29" -bC -killprocess -v
 
-%MyPath%HOTFIXR29SP6\CDROM\B29.6.8.win_b64\1\WIN64\StartHFXB -u "C:\Program Files\Dassault Systemes\B29" -killprocess  -v
-%MyPath%HOTFIXR29SP6\CDROM\B29.6.8.win_b64\1\WIN64\StartHFXB -u "C:\Program Files\Dassault Systemes\B29_RADE" -killprocess  -v
+:RADE
+RADE.win_b64\1\WIN64\StartB -u "C:\Program Files\Dassault Systemes\B29_RADE" -newdir -D "C:\Program Files\Dassault Systemes\B29_RADE\CATEnv" -ident "RADE" -all -v
+SPK.win_b64\1\WIN64\StartSPKB -u "C:\Program Files\Dassault Systemes\B29_RADE" -bC -killprocess -v
+HOTFIXR29SP6\CDROM\B29.6.8.win_b64\1\WIN64\StartHFXB -u "C:\Program Files\Dassault Systemes\B29_RADE" -killprocess  -v
 
+:: "C:\Program Files\Dassault Systemes\B29\win_b64\code\bin\CATSoftwareMgtB" -hfxR
+:: "C:\Program Files\Dassault Systemes\B29_RADE\win_b64\code\bin\CATSoftwareMgtB" -hfxR
 
-
+pause
