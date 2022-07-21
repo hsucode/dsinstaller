7edd55b487eef0739b9c332260e7699e836c74e0
song_hsu <song_hsu@163.com>
2022-07-22 00:24:09 +0800

    delete

--- 3DEXPERIENCE_Platform_V6R2022x_Installer/3DEXPERIENCE_Platform_V6R2022x/USER_CREATION/01-CREAT_3DPASSPORT.bat
+++
@@ -1,8 +0,0 @@
-@echo on
-set mypath=%~dp0
-
-set version=R2021x
-
-:passport
-cd E:\DassaultSystemes\%version%\3DPassport\win_b64\code\command
-PassportUserImport.bat -file %mypath%3DPassportUserCreationScript.txt -default_password Aa123456 -url https://dsplm21x.3ds.com/3dpassport
